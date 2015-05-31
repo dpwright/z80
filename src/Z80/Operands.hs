fcdee3c2817e76e35e33067396792650f638982f
Daniel P. Wright <dani@dpwright.com>
2015-05-31 17:48:14 +0900

    Move the encoding machinery into Z80.Operations
    It's totally local to this module, so no point in having it in Operands and exposing it

--- src/Z80/Operands.hs
+++ src/Z80/Operands.hs
@@ -3,10 +3,6 @@
 module Z80.Operands
   ( -- * Generic Registers
     Reg8 (..)
-  , Reg16
-  , EncodeReg8 (..)
-  , EncodeReg16 (..)
-  , encodeOrError
     -- * Special Registers
   , A (..), F (..), I (..), R (..)
   , BC (..), DE (..), HL (..)
@@ -75,47 +71,3 @@ compatibleIx (Just i) (Just i') =
   error $ "Cannot add two indices: " ++ show i ++ " and " ++ show i'
 
 type Location = Word16
-
-class EncodeReg8 r where
-  encodeReg8 :: r -> Word8
-
-instance EncodeReg8 A where
-  encodeReg8 A = 0x7 -- 111
-
-instance EncodeReg8 Reg8 where
-  encodeReg8 B = 0x0 -- 000
-  encodeReg8 C = 0x1 -- 001
-  encodeReg8 D = 0x2 -- 010
-  encodeReg8 E = 0x3 -- 011
-  encodeReg8 H = 0x4 -- 100
-  encodeReg8 L = 0x5 -- 101
-
-class EncodeReg16 r where
-  encodeReg16 :: r -> Word8
-
-instance EncodeReg16 BC where
-  encodeReg16 BC = 0x0 -- 00
-instance EncodeReg16 DE where
-  encodeReg16 DE = 0x1 -- 01
-instance EncodeReg16 HL where
-  encodeReg16 HL = 0x2 -- 10
-
--- SP and AF both encode to the same value.
--- This will break if there is ever a situation in which either could be passed.
-instance EncodeReg16 SP where
-  encodeReg16 SP = 0x3 -- 11
-instance EncodeReg16 AF where
-  encodeReg16 AF = 0x3 -- 11
-
-class EncodeReg16 r => Reg16 r
-instance Reg16 BC
-instance Reg16 DE
-
-instance EncodeReg16 RegIx where
-  encodeReg16 IX = 0xdd
-  encodeReg16 IY = 0xfd
-  encodeReg16 i  = error $ "Cannot encode offset index: " ++ show i
-
-encodeOrError :: EncodeReg16 x => Maybe x -> Word8
-encodeOrError (Just x) = encodeReg16 x
-encodeOrError Nothing  = error $ "Cannot encode register: no value"
