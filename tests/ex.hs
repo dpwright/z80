2a04ec1f8c67aea013018695e99d71e8011553ec
Daniel P. Wright <dani@dpwright.com>
2015-06-05 09:00:32 +0900

    Rewrite example to be a bit more haskelly

--- tests/ex.hs
+++ tests/ex.hs
@@ -9,16 +9,16 @@ import qualified Data.ByteString as BS
 
 ex :: IO ()
 ex = BS.writeFile "test.bin" . asmData . org 0x6000 $ mdo
-  ld a 2               -- upper screen
-  call 5633            -- open channel
-  loop                 <- label
-  ld de string         -- address of string
-  ld bc (eostr-string) -- length of string to print
-  call 8252            -- print our string
-  jp loop              -- repeat until screen is full
+  ld a 2                 -- upper screen
+  call 5633              -- open channel
 
-  string                <- label
-  db "Dani is cool"
-  eostr                 <- label
+  withLabel $ \loop -> do
+    ld de string         -- address of string
+    ld bc (eostr-string) -- length of string to print
+    call 8252            -- print our string
+    jp loop              -- repeat until screen is full
+
+  string <- labelled $ db "Dani is cool"
+  eostr  <- label
 
   return ()
