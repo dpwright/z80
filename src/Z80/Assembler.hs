e1cd147016d6fe7c5a0599c28dd353b251aae6e1
Daniel P. Wright <dani@dpwright.com>
2015-07-09 07:07:31 +0900

    Add support for statics
    Statics are blocks of ASM which are placed at the end, after your main block,
    and which are defined and accessed using a string identifier.  They allow you
    to break the program apart into multiple Haskell functions without having to
    link those functions in manually.  You do have to be a bit careful with them
    though.

--- src/Z80/Assembler.hs
+++ src/Z80/Assembler.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RecursiveDo #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
 module Z80.Assembler
@@ -14,12 +15,16 @@ module Z80.Assembler
   , label
   , labelled
   , withLabel
+  , static
   , end
   ) where
 
 import Data.Word
 import qualified Data.ByteString as BS
 import Data.ByteString (ByteString)
+import qualified Data.Map.Lazy as M
+import Data.Map.Lazy (Map)
+import Data.Maybe
 
 import Control.Monad.RWS
 
@@ -31,10 +36,12 @@ import Z80.Operands
 
 data ASMState
   = ASMState
-  { loc :: Location
+  { loc        :: Location
+  , statics    :: Map String Z80ASM
+  , staticLocs :: Map String Location
   }
 
-newtype Z80 a = Z80 (RWS () ByteString ASMState a)
+newtype Z80 a = Z80 { runZ80 :: RWS () ByteString ASMState a }
   deriving (Functor, Applicative, Monad, MonadFix)
 type Z80ASM = Z80 ()
 
@@ -82,12 +89,29 @@ withLabel asm = do
   l <- label
   asm l
 
+static :: String -> Z80ASM -> Z80 Location
+static name asm = do
+  Z80 $ modify addIfMissing
+  loc <- M.lookup name . staticLocs <$> Z80 get
+  return $ fromMaybe notFoundError loc
+  where notFoundError   = error $ "Static \"" ++ name ++ "\" not found.  This is probably a bug in the z80 package, please report."
+        addIfMissing st = if M.member name $ statics st then st
+                          else st { statics = M.insert name asm $ statics st }
+
 end :: Z80ASM
 end = return ()
 
 org :: Location -> Z80ASM -> ASMBlock
-org addr (Z80 mc) = ASMBlock { asmOrg = addr, asmEntry = addr, asmData = asm }
- where ((), _, asm) = runRWS mc () (ASMState addr)
+org addr userASM = ASMBlock { asmOrg = addr, asmEntry = addr, asmData = asm }
+ where ((), _, asm)      = runRWS (runZ80 runWithStatics) () initialState
+       initialState      = ASMState addr M.empty M.empty
+       putStatics sls st = st { staticLocs = sls }
+       runWithStatics    = mdo
+         Z80 . modify $ putStatics labelledStatics
+         userASM
+         collectedStatics <- statics <$> Z80 get
+         labelledStatics  <- traverse labelled collectedStatics
+         end
 
 equ :: a -> Z80 a
 equ = return
